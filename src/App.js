9a28715d92aa3baf089aa45b3f41558d283b0ac2
qrisma <nobel77.lee@gmail.com>
2023-11-20 11:43:34 +0900

    v1 : 기본적인 useEffect 사용법 적용 코드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useEffect as defEffect } from "react";
+import { useEffect as countEffect } from "react";
+import { useEffect as nameEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState(1);
+
+  const handleCountUpdate = () => {
+    setCount(count + 1);
+  };
+
+  const handleInputChage = (e) => {
+    setName(e.target.value);
+  };
+
+  // mount 시에만 실행 : 빈 연관배열 설정
+  defEffect(() => {
+    console.log("마운팅 🚀");
+  }, []);
+
+  // 기본적으로 렌더링 될때(mount, 상태변경 시)마다 실행
+  defEffect(() => {
+    console.log("기본적으로 렌더링 될때(mount, 상태변경 시)마다 실행🎨");
+  });
+  // mount 시와 연관배열 요소 인 count의 상태 값 변경 시 만 렌더링 실행
+  countEffect(() => {
+    console.log("⭐count 변경");
+  }, [count]);
+  // mount 시와 연관배열 요소 인 ⭐name의 상태 값 변경 시 만 렌더링 실행
+  nameEffect(() => {
+    console.log("😜name 변경");
+  }, [name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count:{count}</span>
+      <br />
+      <input type="text" onChange={handleInputChage} />
+      <span>name : {name}</span>
     </div>
   );
 }
